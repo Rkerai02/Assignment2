bc6ba6d4dea9a9f224f6c6eba1777a9610ff58bd
Rkerai02 <158575195+Rkerai02@users.noreply.github.com>
2024-02-11 11:08:49 +0530

    Add files via upload

---
+++ modul4-solution/script.js
@@ -0,0 +1,17 @@
+(function () {
+
+    var names = ["Yaakov", "John", "Jeson", "Jagdish", "Paul", "pratik", "Rehan", "Paula", "Laura", "Jim"];
+    
+    for (var i = 0; i < names.length; i++) {
+      var firstLetter = names[i].charAt(0).toLowerCase();
+    
+      if (firstLetter === 'j') {
+        byeSpeaker(names[i]);
+      }
+      else {
+        helloSpeaker(names[i]);
+      }
+    }
+    
+    })();
+    
